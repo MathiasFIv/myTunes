466726d7152148c5ce2db457d88d7daeb7c1a1ec
Morten Heilskov Jensen <morjen05@easv365.dk>
2024-11-29 12:21:14 +0100

    IPlayListDataAccess

---
+++ src/main/java/tv/safte/truemytunes/DAL/IPlayListDataAccess.java
@@ -0,0 +1,19 @@
+package tv.safte.truemytunes.DAL;
+
+//Project Imports
+import tv.safte.truemytunes.BE.Song;
+
+//Java import
+import java.util.List;
+
+public interface IPlayListDataAccess {
+
+    List<PlayList> getAllPlayLists() throws Exception;
+
+    PlayList createPlayList(PlayList newplaylist) throws Exception;
+
+    void updatePlayList(PlayList playList) throws Exception;
+
+    void deletePlayList(PlayList playList) throws Exception;
+
+}
